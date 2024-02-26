6655d3b245bda4ff0e0a86a162303df1f2f6c9a5
Wecreds <wecredsbr@gmail.com>
2024-02-26 11:31:54 -0300

    estlização básica

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.e5q3igv1m7g"
+    "revision": "0.hij3rc7q2to"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
